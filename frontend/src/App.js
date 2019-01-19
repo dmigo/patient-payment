75a799a89a37f40488540750de7c5bec67726a88
dmigo <d.f.goryunov@gmail.com>
2019-01-19 18:20:45 +0100

    Naive UI with api mock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import 'dress-code/dist/css/dress-code.min.css'
 import './App.css'
 
+import api from './api'
+
 const Menu = () => {
   return <div className="dc-card">Menu</div>
 }
@@ -9,21 +11,52 @@ const Menu = () => {
 const Providers = ({ items }) => {
   return (
     <div>
-      {items.map(provider => (
-        <Provider key={`provider-${provider.id}`} {...provider} />
+      {items.map((provider, index) => (
+        <Provider key={`provider-${index}`} {...provider} />
       ))}
     </div>
   )
 }
 
-const Provider = ({ id }) => {
-  return <div className="dc-card">Item {id}</div>
-}
+const Provider = ({
+  name,
+  address,
+  city,
+  state,
+  zip,
+  hospitalReferralRegionDescription,
+  totalDischarges,
+  averageCoveredCharges,
+  averageTotalPayments,
+  averageMedicarePayments
+}) => (
+  <div className="dc-card">
+    <Field label="Provider Name" value={name} />
+    <Field label="Provider Street Address" value={address} />
+    <Field label="Provider City" value={city} />
+    <Field label="Provider State" value={state} />
+    <Field label="Provider Zip Code" value={zip} />
+    <Field
+      label="Hospital Referral Region Description"
+      value={hospitalReferralRegionDescription}
+    />
+    <Field label="Total Discharges" value={totalDischarges} />
+    <Field label="Average Covered Charges" value={averageCoveredCharges} />
+    <Field label="Average Total Payments" value={averageTotalPayments} />
+    <Field label="Average Medicare Payments" value={averageMedicarePayments} />
+  </div>
+)
 
-const providers = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+const Field = ({ label, value }) => (
+  <div className="inline-edit dc-row">
+    <div className="dc-column dc-column--medium-2">{label}</div>
+    <div className="dc-column dc-column--medium-9">{value}</div>
+  </div>
+)
 
 class App extends Component {
   render() {
+    const providers = api.providrs.find()
     return (
       <div className="dc-page">
         <div className="dc-container">
