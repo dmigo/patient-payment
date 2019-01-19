b9232dac8b547390a67e6862e4507cee9d6f8ac5
dmigo <d.f.goryunov@gmail.com>
2019-01-19 17:46:22 +0100

    Basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import 'dress-code/dist/css/dress-code.min.css'
 import './App.css'
+
+const Menu = () => {
+  return <div className="dc-card">Menu</div>
+}
+
+const Providers = ({ items }) => {
+  return (
+    <div>
+      {items.map(provider => (
+        <Provider key={`provider-${provider.id}`} {...provider} />
+      ))}
+    </div>
+  )
+}
+
+const Provider = ({ id }) => {
+  return <div className="dc-card">Item {id}</div>
+}
+
+const providers = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">Hello, Now!</header>
+      <div className="dc-page">
+        <div className="dc-container">
+          <Menu />
+          <Providers items={providers} />
+        </div>
       </div>
     )
   }
