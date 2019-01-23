5548950977c9ff17bac211f55b798a839c21e46b
dmigo <d.f.goryunov@gmail.com>
2019-01-23 02:58:51 +0100

    Minor UI adjustments

--- frontend/src/Providers/Error.js
+++ frontend/src/Providers/Error.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import './Error.css'
 
 const Error = ({ error }) => {
-  return <div className="dc-card">Error: {error}</div>
+  return (
+    <div className="dc-card error-card">
+      <div className="dc-msg dc-msg--error">
+        <div className="dc-msg__inner">
+          <div className="dc-msg__icon-frame">
+            <i className="dc-icon dc-msg__icon dc-icon--error" />
+          </div>
+
+          <div className="dc-msg__bd">
+            <h1 className="dc-msg__title">Error message</h1>
+            <p className="dc-msg__text">Error: {error}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Error
