cfec667ee742386c7a62862adcfbcdab399a5a83
dmigo <d.f.goryunov@gmail.com>
2019-01-23 23:10:05 +0100

    Handle errors

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler')
 
 const port = process.env.PORT || 3000
 const app = express()
 
 const ProvidersController = require('./providers')
+const { errorHandler } = require('./errors')
 
-app.get('/providers', async (req, res) => {
-  const providers = await ProvidersController.find(req.query)
-  res.json(providers)
-})
+app.get(
+  '/providers',
+  asyncHandler(async (req, res, next) => {
+    const providers = await ProvidersController.find(req.query)
+    res.json(providers)
+  })
+)
+
+app.use(errorHandler)
 
 app.listen(port, err => {
   if (err) throw err
